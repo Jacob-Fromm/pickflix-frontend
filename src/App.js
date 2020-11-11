19731429312274c2f00e1f9aa81833e03054eccf
jakeFromm <jacobfromm@gmail.com>
2020-11-11 17:04:51 -0500

    users persisting

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ import {Route} from 'react-router-dom'
 import LogIn from './Welcome Page/LogIn';
 import Signup from './Welcome Page/Signup';
 import axios from "axios";
+import ls from "local-storage"
 
 
-class App extends Component {
-
+class App extends React.Component {
   state = {
     currentUser: ""
   }
+  // const [user, setUser] = React.useState(
+  //   localStorage.getItem(currentUser)
+  // )
+
+  // React.useEffect(() => {
+  //   localStorage.setItem("currentUser", JSON.stringify({currentUser}))
+  // })
+
+  componentDidMount = () => {
+    this.setState({ currentUser: ls.get("currentUser") || "" })
+  }
   
   signupSubmitHandler = (newUser) => {
+    console.log("new user in app.js", newUser)
     fetch("http://localhost:3000/users", {
       method: "POST",
       headers: {
@@ -26,10 +38,15 @@ class App extends Component {
       body: JSON.stringify(newUser)
     })
       .then(r => r.json())
-      .then(user => this.setState({ currentUser: user }))
+      .then(user => {
+        console.log(user)
+        this.setState({ currentUser: user})
+        ls.set("currentUser", user)
+      })
 
   }
   render() {
+    console.log("current user", ls.get("currentUser"))
   return (
     <div className="root" >
       <div >
@@ -38,13 +55,13 @@ class App extends Component {
         <Route 
           path="/pickpage" 
           render={(props) => (
-            <PickPage {...props} currentUser={this.state.currentUser}/>
+            <PickPage {...props} currentUser={ls.get("currentUser")}/>
           )}
           />
         <Route path="/welcome" component={Welcome}  />
         <Route path="/profile" 
           render={(props) => (
-            <Profile {...props} currentUser={this.state.currentUser} />
+            <Profile {...props} currentUser={ls.get("currentUser")} />
           )} />
         <Route path="/login" component={LogIn} />
         <Route 
--- src/Welcome Page/Welcome.jsx
+++ src/Welcome Page/Welcome.jsx
@@ -7,25 +7,25 @@ const Welcome = () => {
 
     return (
         <div>
-            
+
             <div>
-                    <div id="vertical-flip" class="card">
-                        <div class="flip">
-                            <div class="front">
-                                <div class="logo">
-                                    <img src="https://fontmeme.com/temporary/d5947c25c8c66e59884ec5b36ad1d770.png" alt="PickFlix" />
-                                </div>
-                            </div>
-                            <div className="back">
-                                <Link to="/login">
-                                    <Button variant="contained" class="welcomebutton" >Log In</Button>
-                                </Link>
-                                <Link to="/signup">
-                                    <Button variant="contained" class="welcomebutton" >New User?</Button>
-                                </Link>
+                <div id="vertical-flip" class="welcomecard">
+                    <div class="flip">
+                        <div class="front">
+                            <div class="logo">
+                                <img src="https://fontmeme.com/temporary/d5947c25c8c66e59884ec5b36ad1d770.png" alt="PickFlix" />
                             </div>
                         </div>
+                        <div className="back">
+                            <Link to="/login">
+                                <Button variant="contained" class="welcomebutton" >Log In</Button>
+                            </Link>
+                            <Link to="/signup">
+                                <Button variant="contained" class="welcomebutton" >New User?</Button>
+                            </Link>
+                        </div>
                     </div>
+                </div>
             </div>
         </div>
     )
